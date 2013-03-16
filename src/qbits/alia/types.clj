b7294db5bc344525654a4505b7a0fd87def8075a
Max Penet <m@qbits.cc>
2013-03-16 22:10:51 +0100

    add types.clj file

---
+++ src/qbits/alia/types.clj
@@ -0,0 +1,28 @@
+(ns qbits.alia.types
+  (:require
+   [clojure.core.typed :as t]
+   ;; [qbits.hayt.types :refer :all]
+   )
+  (:import
+   [clojure.lang Named IDeref IPersistentMap] ;; the IPMap need to be replace with HaytQuery Type
+    [com.datastax.driver.core
+    PreparedStatement
+    Query]))
+
+(t/def-alias Sequential* (TFn [[x :variance :covariant]]
+                              (U (Vector* x)
+                                 (List* x)
+                                 (Seq* x))))
+
+;; Types
+(t/def-alias ConsistencyValue (U ':one ':two ':three ':quorum ':local
+                                 ':local-quorum ':each-quorum))
+
+(t/def-alias CQLQuery (U Query
+                         PreparedStatement
+                         String
+                         (IPersistentMap Named Any))) ;; replace with HaytQuery Type
+
+(t/def-alias ColumnName (U String Named))
+(t/def-alias CQLResult (t/Option '{ColumnName Any}))
+(t/def-alias CQLAsyncResult (IDeref CQLResult))
